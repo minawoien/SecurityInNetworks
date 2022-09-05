01af45974cc1a195c347e761826d197c8e70c2df
minawoien <mc.woien@stud.uis.no>
2022-09-05 11:38:47 +0200

    express numerically

--- assignment1/part1.py
+++ assignment1/part1.py
@@ -1,3 +1,13 @@
-# Receives the encrypted text and removes the spaces
+# Receives the encrypted text, removes the spaces and covert to lowercase
 encrypted = input("Write in your encrypted text: ")
-encrypted = encrypted.replace(" ", "")
+encrypted = encrypted.replace(" ", "").lower()
+
+key_length = int(input("Key length: "))
+
+# Encrypted text expressed numerically
+encrypted_numeric = []
+for letter in encrypted:
+    encrypted_numeric.append(ord(letter)-97)
+    
+for i in encrypted_numeric:
+    print(i)
