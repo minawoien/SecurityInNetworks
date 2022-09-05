67100ea9621afe652608f5dde5e5c473d1d84347
minawoien <mc.woien@stud.uis.no>
2022-09-05 17:34:35 +0200

    sequence of 2

--- assignment1/part1.py
+++ assignment1/part1.py
@@ -2,12 +2,30 @@
 encrypted = input("Write in your encrypted text: ")
 encrypted = encrypted.replace(" ", "").lower()
 
-key_length = int(input("Key length: "))
+#key_length = int(input("Key length: "))
 
 # Encrypted text expressed numerically
 encrypted_numeric = []
 for letter in encrypted:
     encrypted_numeric.append(ord(letter)-97)
-    
-for i in encrypted_numeric:
-    print(i)
+
+# Find the highest repeated sequence
+sequences = []
+for i in range(len(encrypted_numeric)):
+    if i+1 < len(encrypted_numeric):
+        numbers = []
+        numbers.append(encrypted_numeric[i])
+        numbers.append(encrypted_numeric[i+1])
+        sequences.append(numbers)
+
+for i in range(0, len(sequences), 2):
+    for j in range(2, len(sequences)+1, 2):
+        print(sequences[i])
+        print(sequences[j])
+        if sequences[i] == sequences[j]:
+            print("ja")
+
+
+
+# for i in encrypted_numeric:
+#     print(i)
