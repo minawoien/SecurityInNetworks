b573fd838d5eacf89636148807bb739e167bf3f5
minawoien <mc.woien@stud.uis.no>
2022-10-20 14:50:52 +0200

    establish connection nodes

--- assignment3/part1/node.py
+++ assignment3/part1/node.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, request
 from argparse import ArgumentParser
+import requests, json
 
 app = Flask(__name__)
 
+addressList = []
+
+def set_addresses(arg):
+    addressList = arg.split(",")
+    return addressList
+
 @app.route("/")
 def index():
+    establish_con()
     return "Hello world"
+
+@app.route("/", methods=["POST", "GET"])
+def establish_con():
+    if len(addressList) > 1:
+        requests.post("http://localhost:"+addressList[1], json={"msg": addressList[0]})
+    else:
+        data = request.get_json()
+        print(data)
+    return "Hi"
 
 
 if __name__ == "__main__":
@@ -14,4 +32,5 @@ if __name__ == "__main__":
     parser.add_argument('-a')
     args = parser.parse_args()
     arg = args.a
-    app.run(debug=True, host='127.0.0.1')
+    addressList = set_addresses(arg)
+    app.run(debug=True, host='127.0.0.1', port=addressList[0])
