b07178e09f2128f74f5bf15459bd1d3d1af24738
minawoien <mc.woien@stud.uis.no>
2022-10-27 11:52:52 +0200

    Node connecting when entering the network

--- assignment3/README.md
+++ assignment3/README.md
@@ -4,6 +4,9 @@
 `flask run --host 0.0.0.0 --port 8080`
 
 
+## How to run
+python3 node.py -host 127.0.0.1:8080
+python3 node.py -host 127.0.0.1:5001 -r 127.0.0.1:8080
 
 
 https://stackoverflow.com/questions/48346025/how-to-pass-an-arbitrary-argument-to-flask-through-app-run
--- assignment3/node.py
+++ assignment3/node.py
@@ -5,32 +5,66 @@ import requests, json
 
 app = Flask(__name__)
 
-addressList = []
+class HashTable:
+    def __init__(self):
+        self.host = None
+        self.hashTable = {}
+    
+    def set_address(self, socket, port):
+        self.hashTable[socket] = port
+    
+    def check_address(self, address):
+        if address not in self.hashTable:
+            address_info = address.split(":")
+            self.set_address(address, address_info[1])
+            return True
+        return False
+    
 
-def set_addresses(arg):
-    addressList = arg.split(",")
-    return addressList
+hash = HashTable()
+# hashTable = {}
+
+# def set_addresses(socket, port):
+#     hashTable[socket] = port
 
 @app.route("/")
 def index():
-    establish_con()
     return "Hello world"
 
+# Receive the address of a node on the network, check if it is in its own hash table and adds it
 @app.route("/", methods=["POST", "GET"])
 def establish_con():
-    if len(addressList) > 1:
-        requests.post("http://localhost:"+addressList[1], json={"msg": addressList[0]})
-    else:
-        data = request.get_json()
-        print(data)
+    data = request.get_json()
+    address = data['msg']
+    if not hash.check_address(address):
+        address_info = address.split(":")
+        print(address_info[1])
+        connect(address_info[1])
+    print(data)
     return "Hi"
 
+# Connect with the remote host and sends its own address
+def connect(port):
+    requests.post("http://localhost:"+port, json={"msg": hash.host})
+    print("done")
 
 if __name__ == "__main__":
-    # Insert the addresses
+    # Insert the sockets
     parser = ArgumentParser()
-    parser.add_argument('-a')
+    parser.add_argument('-host')
+    parser.add_argument('-r')
     args = parser.parse_args()
-    arg = args.a
-    addressList = set_addresses(arg)
-    app.run(debug=True, host='127.0.0.1', port=addressList[0])
+
+    # Split the socket into host and port, and add it to the hash table
+    host = args.host.split(":")
+    hash.host = args.host
+    hash.set_address(args.host, host[1])
+
+    # If an remote socket is added as parameter, the socket is splitted into socket and port and added to the hash table
+    # Then the host will connect with the remote host
+    if args.r is not None:
+        remote = args.r.split(":")
+        hash.set_address(args.r, remote[1])
+        connect(remote[1])
+
+    app.run(debug=True, host=host[0], port=host[1])
