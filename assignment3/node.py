66466de19c4e61c0e3d18b0ad25afbd341f6a051
minawoien <mc.woien@stud.uis.no>
2022-10-28 09:36:26 +0200

    separate routing table

--- assignment3/node.py
+++ assignment3/node.py
@@ -1,38 +1,60 @@
 from time import sleep
+from urllib import response
 from flask import Flask, request, redirect, url_for
 from argparse import ArgumentParser
-from hashtable import HashTable
+from routing import RoutingTable
 import requests
 
 app = Flask(__name__)
 
-hash = HashTable()
+routing = RoutingTable()
 
 @app.route("/")
 def index():
     return "Hello world"
 
-# Receive the address of a node on the network, check if it is in its own hash table and adds it
+# Receive the address of a node on the network, check if it is in its own routing table and adds it
 @app.route("/est", methods=["POST"])
 def establish_con():
     data = request.get_json()
-    print(data)
     address = data['msg']
-    print(hash.hashTable)
-    hash.check_address(address)
-    return str(len(hash.hashTable))
+    print(routing.routing_to_address)
+    routing.check_address(address)
+    return str(len(routing.routing_to_address))
+
+@app.route("/getAll", methods=["GET"])
+def sync():
+    print("inni sync")
+    return "HALLO"
 
 # Connect with the remote host and sends its own address
 def connect(port):
-    response = requests.post(f"http://127.0.0.1:{port}/est", json={"msg": hash.host, "count":len(hash.hashTable)})
+    response = requests.post(f"http://127.0.0.1:{port}/est", json={"msg": routing.host, "count":len(routing.routing_to_address)})
     url = response.url
     first = url.find("/") + 2
     end = first + url[first:].find("/")
-    hash.check_address(url[first:end])
-    print(hash.hashTable)
-    print("Response sin count i hash tabellen:", int(response.text))
-    print("Min hash tabell:", len(hash.hashTable))
+    routing.check_address(url[first:end])
+    print(routing.routing_to_address)
+    print("Response sin count i routing tabellen:", int(response.text))
+    print("Min routing tabell:", len(routing.routing_to_address))
+
+
     # Videre: hvis ulikt tall, må få bedt om den siste
+
+# Function for sending files to all the nodes in the network
+def broadcast():
+    for address in routing.routing_to_address:
+        if routing.host != routing.routing_to_address[address]:
+            response = request.post(f"http://127.0.0.1:{routing.routing_to_address[address]}/getfile")
+            print(response.text)
+
+def update_all(address):
+    port = address.split(":")
+    print(port[1])
+    sleep(9)
+    response = request.get(f"http://127.0.0.1:{port[1]}/getAll")
+    print(response.text)
+
 
 if __name__ == "__main__":
     # Insert the sockets
@@ -41,19 +63,18 @@ if __name__ == "__main__":
     parser.add_argument('-r')
     args = parser.parse_args()
 
-    # Split the socket into host and port, and add it to the hash table
+    # Split the socket into host and port, and add it to the routing table
     host = args.host.split(":")
-    hash.host = args.host
-    hash.set_address(args.host, host[1])
+    routing.host = args.host
+    routing.set_address(args.host, host[1])
 
-    # If an remote socket is added as parameter, the socket is splitted into socket and port and added to the hash table
+    # If an remote socket is added as parameter, the socket is splitted into socket and port and added to the routing table
     # Then the host will connect with the remote host
     if args.r is not None:
         remote = args.r.split(":")
-        hash.set_address(args.r, remote[1])
+        routing.set_address(args.r, remote[1])
         connect(remote[1])
+
     app.run(debug=True, host=host[0], port=host[1])
-
-
 
 
--- assignment3/routing.py
+++ assignment3/routing.py
@@ -1,13 +1,14 @@
-class HashTable:
+class RoutingTable:
     def __init__(self):
         self.host = None
-        self.hashTable = {}
+        self.routing_to_address = {}
+        self.routing_to_ID = {}
     
     def set_address(self, socket, port):
-        self.hashTable[socket] = port
+        self.routing_to_address[socket] = port
     
     def check_address(self, address):
-        if address not in self.hashTable:
+        if address not in self.routing_to_address:
             address_info = address.split(":")
             self.set_address(address, address_info[1])
 
