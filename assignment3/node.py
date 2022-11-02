a1b3d97824880cfdb865b36f2c5c0afc5c9d15db
minawoien <mc.woien@stud.uis.no>
2022-11-02 08:38:15 +0100

    share dht

--- assignment3/node.py
+++ assignment3/node.py
@@ -18,7 +18,14 @@ def upload():
     file = request.files['file']
     hash = dht.create_hash(file)
     dht.add(routing.guid, routing.host, hash, file.filename)
+    updated_dht()
     return app.send_static_file("index.html")
+
+@app.route("/getdht", methods=["POST"])
+def get_dht():
+    data = request.get_json()
+    print(data)
+    return routing.host
 
 @app.route("/getNodes", methods=["GET"])
 def getNodes():
@@ -45,9 +52,9 @@ def connect(address):
     first = url.find("/") + 2
     end = first + url[first:].find("/")
     routing.check_address(response.text, url[first:end])
-    get_all(address)
+    share_tables(address)
 
-def get_all(address):
+def share_tables(address):
     routing_table = requests.get(f"http://{address}/routingTable").content
     routing_table = json.loads(routing_table)
     for guid in routing_table:
@@ -56,6 +63,11 @@ def get_all(address):
             requests.post(f"http://{new_address}/est", json={"address": routing.host, "guid": routing.guid})
     for i in routing.routing_to_ID:
         print(i)
+
+def updated_dht():
+    for address in routing.routing_to_ID:
+        if address != routing.host:
+            requests.post(f"http://{address}/getdht", json={"file": dht.hashTable})
    
 if __name__ == "__main__":
     routing = RoutingTable()
